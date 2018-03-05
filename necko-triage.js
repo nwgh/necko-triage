b344153dca449c68cb671aa70797dbdc9c5f3616
Nicholas Hurley <hurley@mozilla.com>
2018-03-05 14:18:04 -0800

    Version bump

--- necko-triage.js
+++ necko-triage.js
@@ -301,7 +301,7 @@ AppSettings.prototype.get = function (key) {
 NeckoTriage = function () { };
 NeckoTriage.prototype.tables = {};
 NeckoTriage.prototype.rootElement = "#necko-triage-root";
-NeckoTriage.prototype.version = "0.0.4";
+NeckoTriage.prototype.version = "0.0.5";
 NeckoTriage.prototype.useTabs = false;
 NeckoTriage.prototype.availableTables = {
     "untriaged-no-ni": {
